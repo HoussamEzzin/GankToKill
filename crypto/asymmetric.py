9c155571377f27ea34bd83693aa9cd82c7c7d1b6
Ezzin Houssam <ezzinhoussam@gmail.com>
2022-01-13 21:51:28 +0100

    crypto testing

---
+++ crypto/asymmetric.py
@@ -0,0 +1,14 @@
+import rsa
+
+publicKey, privateKey = rsa.newkeys(521)
+
+message = "hello world"
+
+encMessage = rsa.encrypt(message.encode(), publicKey)
+
+print("original string: ", message)
+print("encrypted string: ", encMessage)
+
+decMessage = rsa.decrypt(encMessage,privateKey).decode()
+
+print("decrypted string: ", decMessage)

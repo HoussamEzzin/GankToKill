be8d4559181cd5b4edc5bdf699e8f7755aadfa62
Ezzin Houssam <ezzinhoussam@gmail.com>
2022-01-25 22:44:57 +0100

    getting name from number bug

--- gathering info/phone.py
+++ gathering info/phone.py
@@ -5,7 +5,7 @@ phone_number = input("Phone number with country code : ")
 phone_number = phonenumbers.parse(phone_number)
 
 print(timezone.time_zones_for_number(phone_number))
-print(carrier.name_for_number(phone_number,"en"))
+print(carrier.name_for_number(phone_number,"ma"))
 print(geocoder.description_for_number(phone_number,"en"))
 
 print("Valid phone number : ", phonenumbers.is_valid_number(phone_number))

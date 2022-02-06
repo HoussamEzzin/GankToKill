2d7ebb56020ebd413609e4e3553c083f06b939ab
Ezzin Houssam <ezzinhoussam@gmail.com>
2022-02-06 21:32:45 +0100

    up to date

--- menu.py
+++ menu.py
@@ -1,3 +1,9 @@
+import coloredlogs, logging
+
+
+logger = logging.getLogger(__name__)
+coloredlogs.install(fmt='%(message)s',level='DEBUG', logger=logger)
+
 
 def menu():
     print("*****GANK TO KILL*****")
